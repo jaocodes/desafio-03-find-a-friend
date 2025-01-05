04e63efca08c8de3a02bcd2a261d0d483379bd9a
jaocodes <jhonskaz@gmail.com>
2025-01-05 14:21:33 -0300

    Setup project

---
+++ prisma/migrations/20250105172018_add_org_and_pet_tables/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "coordinator_name" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "energy_stat" TEXT NOT NULL,
+    "space_requirement" TEXT NOT NULL,
+    "orgId" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "orgs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/env/index.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+const envShema = z.object({
+    PORT: z.coerce.number().default(3333),
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+})
+
+const _env = envShema.safeParse(process.env)
+
+if (_env.success === false) {
+    console.error('❌ Invalid environment variables', _env.error.format())
+    throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
