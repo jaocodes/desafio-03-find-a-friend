8a244d954e8d534360bbbfa19deca44cd2cbaea2
jaocodes <jhonskaz@gmail.com>
2025-01-06 20:23:23 -0300

    adiciona repositório em memória para pets e testes do caso de uso de criação de pets

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,25 @@
+import type { Prisma, Pet } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+import type { PetsRepository } from '../pets-repository'
+
+export class InMemoryPetsRepository implements PetsRepository {
+    private items: Pet[] = []
+    async create(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
+        const pet: Pet = {
+            id: data.id ?? randomUUID(),
+            name: data.name,
+            about: data.about,
+            age: data.age,
+            animal_type: data.animal_type,
+            energy_stat: data.energy_stat,
+            size: data.size,
+            orgId: data.orgId,
+            space_requirement: data.space_requirement,
+        }
+
+        this.items.push(pet)
+
+        return pet
+    }
+}
---
+++ src/use-cases/create-pet.spec.ts
@@ -0,0 +1,65 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { CreatePetUseCase } from './create-pet'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import type { OrgsRepository } from '@/repositories/orgs-repository'
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
+import { hash } from 'bcryptjs'
+import { ResourseNotFoundError } from './errors/resource-not-found-error'
+
+let petsRepository: InMemoryPetsRepository
+let orgsRepository: OrgsRepository
+let sut: CreatePetUseCase
+
+describe('Create a pet use-case', () => {
+    beforeEach(async () => {
+        petsRepository = new InMemoryPetsRepository()
+        orgsRepository = new InMemoryOrgsRepository()
+        sut = new CreatePetUseCase(petsRepository, orgsRepository)
+
+        await orgsRepository.create({
+            id: 'org-id',
+            name: 'Happy Pet',
+            coordinator_name: 'Jhon Cruz',
+            whatsapp: '91984087807',
+            email: 'jhon@example.com',
+            password_hash: await hash('12345678', 6),
+            cep: '68743200',
+            state: 'PA',
+            city: 'Castanhal',
+            street: 'Barão do Rio Branco',
+            number: '38-B',
+            latitude: -1.2843669,
+            longitude: -47.9242824,
+        })
+    })
+
+    it('should be able to create a pet', async () => {
+        const { pet } = await sut.execute({
+            name: 'Floquinho',
+            about: 'Sou pequeno e branquinho como um floco de neve',
+            age: '1 ano',
+            energyStat: 'Muita energia',
+            size: 'small',
+            orgId: 'org-id',
+            animalType: 'dog',
+            spaceRequirement: 'Pequeno',
+        })
+
+        expect(pet.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to create a pet if org does not exists', async () => {
+        await expect(() =>
+            sut.execute({
+                name: 'Floquinho',
+                about: 'Sou pequeno e branquinho como um floco de neve',
+                age: '1 ano',
+                energyStat: 'Muita energia',
+                size: 'small',
+                orgId: 'org-fake',
+                animalType: 'dog',
+                spaceRequirement: 'Pequeno',
+            }),
+        ).rejects.toBeInstanceOf(ResourseNotFoundError)
+    })
+})
