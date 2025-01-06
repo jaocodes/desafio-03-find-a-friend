c7ca921af00c033e21aeaae83b9f83c2c86b41c2
jaocodes <jhonskaz@gmail.com>
2025-01-05 22:09:14 -0300

    adiciona testes unitários para o caso de uso de registro de organizações

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,14 +1,20 @@
-import { MakeRegisterUseCase } from '@/factorys/make-register-use-case'
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { RegisterUseCase } from './register'
+import { compare, hash } from 'bcryptjs'
+import { OrgAlreadyExistsError } from './errors/org-already-exists-error'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: RegisterUseCase
 
 describe('Register use-case', () => {
-    it('should be able to register a org', async () => {
-        const orgRepository = new InMemoryOrgsRepository()
-        const registerUsecase = new RegisterUseCase(orgRepository)
+    beforeEach(() => {
+        orgsRepository = new InMemoryOrgsRepository()
+        sut = new RegisterUseCase(orgsRepository)
+    })
 
-        const createdOrg = await registerUsecase.execute({
+    it('should be able to register a org', async () => {
+        const { org } = await sut.execute({
             name: 'Happy Pet',
             coordinator_name: 'Jhon Cruz',
             whatsapp: '91984087807',
@@ -23,6 +29,64 @@ describe('Register use-case', () => {
             longitude: -47.9242824,
         })
 
-        expect(createdOrg.org.id).toEqual(expect.any(String))
+        expect(org.id).toEqual(expect.any(String))
+    })
+
+    it('should hash org password upon registration', async () => {
+        const { org } = await sut.execute({
+            name: 'Happy Pet',
+            coordinator_name: 'Jhon Cruz',
+            whatsapp: '91984087807',
+            email: 'jhon@example.com',
+            password: '12345678',
+            cep: '68743200',
+            state: 'PA',
+            city: 'Castanhal',
+            street: 'Barão do Rio Branco',
+            number: '38-B',
+            latitude: -1.2843669,
+            longitude: -47.9242824,
+        })
+
+        const isPasswordCorrectHashed = await compare(
+            '12345678',
+            org.password_hash,
+        )
+
+        expect(isPasswordCorrectHashed).toBe(true)
+    })
+
+    it('should not be able to register a org with the same email', async () => {
+        await orgsRepository.create({
+            name: 'Happy Pet',
+            coordinator_name: 'Jhon Cruz',
+            whatsapp: '91984087807',
+            email: 'jhon@example.com',
+            password_hash: await hash('12345678', 6),
+            cep: '68743200',
+            state: 'PA',
+            city: 'Castanhal',
+            street: 'Barão do Rio Branco',
+            number: '38-B',
+            latitude: -1.2843669,
+            longitude: -47.9242824,
+        })
+
+        await expect(() =>
+            sut.execute({
+                name: 'Happy Pet',
+                coordinator_name: 'Jhon Cruz',
+                whatsapp: '91984087807',
+                email: 'jhon@example.com',
+                password: '12345678',
+                cep: '68743200',
+                state: 'PA',
+                city: 'Castanhal',
+                street: 'Barão do Rio Branco',
+                number: '38-B',
+                latitude: -1.2843669,
+                longitude: -47.9242824,
+            }),
+        ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
     })
 })
