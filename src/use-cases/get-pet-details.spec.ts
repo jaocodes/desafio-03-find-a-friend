f89a90789498a6ae468ba0fe21dca38c50737c7c
jaocodes <jhonskaz@gmail.com>
2025-01-07 11:57:56 -0300

    adiciona caso de uso e testes para obter detalhes de pets

---
+++ src/use-cases/get-pet-details.spec.ts
@@ -0,0 +1,51 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GetPetDetailsUseCase } from './get-pet-details'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import type { PetsRepository } from '@/repositories/pets-repository'
+import { ResourseNotFoundError } from './errors/resource-not-found-error'
+
+let petsRepository: PetsRepository
+let sut: GetPetDetailsUseCase
+
+describe('Get pet details use-case', () => {
+    beforeEach(() => {
+        petsRepository = new InMemoryPetsRepository()
+        sut = new GetPetDetailsUseCase(petsRepository)
+    })
+
+    it('should be able to get pet details', async () => {
+        await petsRepository.create({
+            id: 'some-id',
+            name: 'Floquinho',
+            about: 'Sou pequeno e branquinho como um floco de neve',
+            age: '1 ano',
+            energy_stat: 'Muita energia',
+            size: 'small',
+            orgId: 'org-id',
+            animal_type: 'dog',
+            space_requirement: 'Pequeno',
+        })
+
+        const { pet } = await sut.execute({ petId: 'some-id' })
+
+        expect(pet.name).toEqual('Floquinho')
+    })
+
+    it('should not be able to get pet details with invalid id', async () => {
+        await petsRepository.create({
+            id: 'some-id',
+            name: 'Floquinho',
+            about: 'Sou pequeno e branquinho como um floco de neve',
+            age: '1 ano',
+            energy_stat: 'Muita energia',
+            size: 'small',
+            orgId: 'org-id',
+            animal_type: 'dog',
+            space_requirement: 'Pequeno',
+        })
+
+        await expect(() =>
+            sut.execute({ petId: 'wrong-id' }),
+        ).rejects.toBeInstanceOf(ResourseNotFoundError)
+    })
+})
---
+++ src/use-cases/get-pet-details.ts
@@ -0,0 +1,25 @@
+import type { PetsRepository } from '@/repositories/pets-repository'
+import type { Pet } from '@prisma/client'
+import { ResourseNotFoundError } from './errors/resource-not-found-error'
+
+interface GetPetDetailsUseCaseRequest {
+    petId: string
+}
+
+interface GetPetDetailsUseCaseResponse {
+    pet: Pet
+}
+
+export class GetPetDetailsUseCase {
+    constructor(private petsRepository: PetsRepository) {}
+
+    async execute({
+        petId,
+    }: GetPetDetailsUseCaseRequest): Promise<GetPetDetailsUseCaseResponse> {
+        const pet = await this.petsRepository.findById(petId)
+
+        if (!pet) throw new ResourseNotFoundError()
+
+        return { pet }
+    }
+}
